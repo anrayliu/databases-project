b71fcb7b1570be25488cb3fe2a3c6100edd18898
Anray <anrayliu@gmail.com>
2025-03-21 02:27:12 -0400

    commit rest of files

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
-#!/home/anray/FlaskApp/venv/bin/python3
-
-
+import os
+import psycopg2
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+def get_db_conn():
+	conn = psycopg2.connect(database="defaultdb",
+							host="pg-147bd20e-databases-project.h.aivencloud.com",
+							port="17637",
+							user=os.environ["DB_USERNAME"],
+							password=os.environ["DB_PASSWORD"])
+	return conn
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	conn = get_db_conn()
+	cur = conn.cursor()
+	cur.execute("select * from public.hotel_chain")
+	hotel_chains = cur.fetchall()
+	cur.close()
+	conn.close()
+
+	return render_template("index.html", hotel_chains=hotel_chains)
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run()
--- readme.md
+++ readme.md
@@ -1,3 +1,16 @@
 # Database Project
 
 ## Anray Liu, Adam R, Val
+
+
+
+
+WHAT YOU NEED TO KNOW:
+
+1. Use Linux.
+
+2. To launch the program, you need to run `app.py` with the included Python virtual environmnent. Activate your venv with `source venv/bin/activate` and run it with `python3 app.py`. Before running and every time you open a new shell, run `export DB_USERNAME=""` and `export DB_PASSWORD=""`. I sent the credentials in discord (wow look at us implementing security measures for quite literally nobody)
+
+3. After launching, type `localhost:5000` into your web browser to see the amazing webpage!!!11!1!!
+
+4. There is a live cloud hosted postgresql db running right now. init_db.py is used to add initial values to the db only, and isn't used for the website.
