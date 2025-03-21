c6f9e430d8e0f3da4d1e149fddafb74bce5b1079
Anray <anrayliu@gmail.com>
2025-03-21 13:55:09 -0400

    add db post

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import psycopg2
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -12,16 +12,26 @@ def get_db_conn():
 							password=os.environ["DB_PASSWORD"])
 	return conn
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-	conn = get_db_conn()
-	cur = conn.cursor()
-	cur.execute("select * from public.hotel_chain")
-	hotel_chains = cur.fetchall()
-	cur.close()
-	conn.close()
+	if request.method == "POST":
+		conn = get_db_conn()
+		cur = conn.cursor()
+		cur.execute(f"insert into public.hotel_chain values('{request.form["chain_name"]}', {request.form["num_hotels"]});")
+		conn.commit()
+		cur.close()
+		conn.close()
+		return redirect("/")
 
-	return render_template("index.html", hotel_chains=hotel_chains)
+	else:
+		conn = get_db_conn()
+		cur = conn.cursor()
+		cur.execute("select * from public.hotel_chain")
+		hotel_chains = cur.fetchall()
+		cur.close()
+		conn.close()
+
+		return render_template("index.html", hotel_chains=hotel_chains)
 
 
 if __name__ == "__main__":
