d705284d6bb710f7e7c357039943627e8b8149fd
Adam R <adam@rosenblatt.ca>
2025-03-26 16:51:08 -0400

    user constraints added

--- init_db.py
+++ init_db.py
@@ -42,10 +42,12 @@ insert into chain_addresses values('im baked hotels', '53 Sandy Hill Street');
 cur.execute("drop table if exists booking;")
 
 cur.execute('''create table booking (
-                    customr_id int,
+                    customer_id int,
                     booking_id int,
                     renting_id int,
-                    constraint pk_booking primary key (customer_id, booking_id)
+                    constraint pk_booking primary key (customer_id, booking_id),
+                    foreign key (customer_id) references customer (id),
+                    foreign key (renting_id) references renting (renting_id)
                 );
             ''')
 
@@ -56,7 +58,10 @@ cur.execute('''create table renting (
                     ssn int,
                     renting_id int,
                     booking_id int,
-                    constraint pk_renting primary key (customer_id, renting_id)
+                    constraint pk_renting primary key (customer_id, renting_id),
+                    foreign key (customer_id) references customer (id),
+                    foreign key (ssn) references employee (ssn),
+                    foreign key (booking_id) references booking (booking_id)
                 );
             ''')
 
@@ -67,7 +72,10 @@ cur.execute('''create table works_at (
                     hotel_address varchar(30),
                     ssn int, 
                     role varchar(30),
-                    constraint pk_works_at primary key (chain_name, hotel_address, ssn)
+                    constraint pk_works_at primary key (chain_name, hotel_address, ssn),
+                    foreign key (chain_name) references hotel_chain (chain name),
+                    foreign key (hotel_address) references hotel (hotel_address),
+                    foreign key (ssn) references employee (ssn)
                 );
             ''')
 
@@ -77,10 +85,13 @@ cur.execute('''create table room (
                     hotel_address varchar(30),
                     chain_name varchar(30),
                     expandable boolean,
-                    price int,
+                    price float unsigned,
                     capacity int,
                     view varchar(30),
-                    constraint pk_room primary key (hotel_address, chain_name, expandable, price, capacity, view)
+                    constraint pk_room primary key (hotel_address, chain_name, expandable, price, capacity, view),
+                    constraint chk_view check (frequecy in ('mountain', 'ocean')),
+                    foreign key (chain_name) references hotel_chain (chain_name),
+                    foreign key (hotel_address) references hotel (hotel_address)
                 );
             ''')
 
@@ -90,7 +101,9 @@ cur.execute('''create table amenities (
                     chain_name varchar(30),
                     hotel_address varchar(30),
                     amenity varchar(30),
-                    constraint pk_amenities primary key (chain_name, hotel_address, amenity)
+                    constraint pk_amenities primary key (chain_name, hotel_address, amenity),
+                    foreign key (chain_name) references hotel_chain (chain_name),
+                    foreign key (hotel_address) references hotel (hotel_address)
                 );
             ''')
 
@@ -100,7 +113,9 @@ cur.execute('''create table damage (
                     chain_name varchar(30),
                     hotel_address varchar(30),
                     damage varchar(30),
-                    constraint pk_damage primary key (chain_name, hotel_address, damage)
+                    constraint pk_damage primary key (chain_name, hotel_address, damage),
+                    foreign key (chain_name) references hotel_chain (chain_name),
+                    foreign key (hotel_address) references hotel (hotel_address)
                 );
             ''')
 
@@ -109,7 +124,9 @@ cur.execute("drop table if exists booking_history;")
 cur.execute('''create table booking_history (
                     booking_id int,
                     past_booking_id int,
-                    constraint pk_booking_history primary key (booking_id, past_booking_id)
+                    constraint pk_booking_history primary key (booking_id, past_booking_id),
+                    foreign key (booking_id) references booking (booking_id),
+                    foreign key (past_booking_id) references booking (booking_id)
                 );
             ''')
 
@@ -118,7 +135,9 @@ cur.execute("drop table if exists renting_history;")
 cur.execute('''create table renting_history (
                     renting_id int,
                     past_renting_id int,
-                    constraint pk_renting_history primary key (renting_id, past_renting_id)
+                    constraint pk_renting_history primary key (renting_id, past_renting_id),
+                    foreign key (renting_id) references renting (renting_id),
+                    foreign key (past_renting_id) references renting (renting_id)
                 );
             ''')
 
