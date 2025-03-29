9a8eecc517253790ab6b385aab66ece743f87e69
Anray <anrayliu@gmail.com>
2025-03-29 18:12:14 -0400

    almost finish filter for rooms

--- app.py
+++ app.py
@@ -29,11 +29,13 @@ def index():
 		try:
 			cur.execute("select * from hotel_chain")
 			hotel_chains = cur.fetchall()
+			cur.execute("select * from hotel")
+			hotels = cur.fetchall()
 		except psycopg2.Error:
 			conn.rollback()
 			return "something went seriously wrong"
 
-		return render_template("index.html", hotel_chains=hotel_chains)
+		return render_template("index.html", hotels=hotels, hotel_chains=hotel_chains)
 
 
 @app.route("/delete/<string:id>")
@@ -65,6 +67,29 @@ def edit(id):
 
 		return render_template("edit.html", chain=chain)
 
+
+@app.route("/results")
+def show_results():
+	start = request.args.get("start_date")
+	end = request.args.get("end_date")
+	capacity = request.args.get("capacity")
+	total = request.args.get("total_capacity")
+	chain = request.args.get("chain")
+	loc = request.args.get("location")
+	price = request.args.get("price")
+
+	try:
+		cur.execute('''
+					select * from room join hotel on room.chain_name=hotel.chain_name and room.chain_name='{}'
+						and num_rooms={} and capacity={} and hotel_address='{}' and price<{};				
+					'''.format(chain, total, capacity, loc, price))
+		res = cur.fetchall()
+
+	except psycopg2.Error:
+		conn.rollback()
+		return "something went seriously wrong"
+
+	return render_template("results.html", results=res)
 
 
 if __name__ == "__main__":
