7e8ca2bbee879dacec8f78f40f9d49146d3b7f98
Anray <anrayliu@gmail.com>
2025-03-29 16:56:43 -0400

    edit/delete post

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import psycopg2
 from flask import Flask, render_template, request, redirect
 
+
 app = Flask(__name__)
 
 def get_db_conn():
@@ -12,27 +13,61 @@ def get_db_conn():
 							password=os.environ["DB_PASSWORD"])
 	return conn
 
+
 @app.route("/", methods=["POST", "GET"])
-def index():
+def index(): 
 	if request.method == "POST":
-		conn = get_db_conn()
-		cur = conn.cursor()
-		cur.execute("insert into hotel_chain values('{}', {});".format(request.form["chain_name"], request.form["num_hotels"]))
-		conn.commit()
-		cur.close()
-		conn.close()
+		try:
+			cur.execute("insert into hotel_chain values('{}', {});".format(request.form["chain_name"], request.form["num_hotels"]))
+			conn.commit()
+		except psycopg2.Error:
+			conn.rollback()
+
 		return redirect("/")
 
 	else:
-		conn = get_db_conn()
-		cur = conn.cursor()
-		cur.execute("select * from hotel_chain")
-		hotel_chains = cur.fetchall()
-		cur.close()
-		conn.close()
+		try:
+			cur.execute("select * from hotel_chain")
+			hotel_chains = cur.fetchall()
+		except psycopg2.Error:
+			conn.rollback()
+			return "something went seriously wrong"
 
 		return render_template("index.html", hotel_chains=hotel_chains)
 
 
+@app.route("/delete/<string:id>")
+def delete(id):
+	try:
+		cur.execute("delete from hotel_chain where chain_name = '{}'".format(id))
+	except psycopg2.Error:
+		conn.rollback()
+
+	return redirect("/")
+
+
+@app.route("/edit/<string:id>", methods=["GET", "POST"])
+def edit(id):
+	if request.method == "POST":
+		try:
+			cur.execute("update hotel_chain set chain_name='{}', num_hotels='{}' where chain_name='{}'".format(request.form["chain_name"], request.form["num_hotels"], id))
+		except psycopg2.Error:
+			conn.rollback()
+
+		return redirect("/")
+	else:
+		try:
+			cur.execute("select * from hotel_chain where chain_name='{}'".format(id))
+			chain = cur.fetchone()
+		except psycopg2.Error:
+			conn.rollback()
+			return "something went seriously wrong"
+
+		return render_template("edit.html", chain=chain)
+
+
+
 if __name__ == "__main__":
-	app.run()
+	with get_db_conn() as conn:
+		with conn.cursor() as cur:
+			app.run(debug=True)
